d349fbde17bd57927df1b822ed3154e923bb1e35
Lopuf <maxcalio28@gmail.com>
2021-12-18 18:37:51 +0100

    now modelisation should be a lot like the final robot

--- Assets/RobotScript.cs
+++ Assets/RobotScript.cs
@@ -9,13 +9,15 @@ public class RobotScript : MonoBehaviour
     private GameObject joint2;
     private GameObject joint3;
     private GameObject joint4;
+    private GameObject joint5;
     private articulationRotoideScript jointScript1;
     private articulationPrismaticScript jointScript2;
     private articulationRotoideScript jointScript3;
     private articulationPrismaticScript jointScript4;
+    private articulationRotoideScript jointScript5;
 
 
-[System.Serializable]
+    [System.Serializable]
     public struct Stats
     {
         [Header("Movement Settings")]
@@ -28,6 +30,8 @@ public class RobotScript : MonoBehaviour
         public float positionRotoide2;
         [Range(0, 12)]
         public float positionPrismatic2;
+        [Range(-180, 180)]
+        public float positionRotoide3;
     }
     public RobotScript.Stats jointsPositions = new RobotScript.Stats
     {
@@ -35,18 +39,21 @@ public class RobotScript : MonoBehaviour
         positionPrismatic1 = 0,
         positionRotoide2 = 0,
         positionPrismatic2 = 0,
+        positionRotoide3 = 0,
     };
     // Start is called before the first frame update
     void Start()
     {
-        joint1 =  GameObject.Find("Articulation1");
+        joint1 = GameObject.Find("Articulation1");
         joint2 = GameObject.Find("Articulation2");
         joint3 = GameObject.Find("Articulation3");
         joint4 = GameObject.Find("Articulation4");
+        joint5 = GameObject.Find("Articulation5");
         jointScript1 = joint1.GetComponent<articulationRotoideScript>();
         jointScript2 = joint2.GetComponent<articulationPrismaticScript>();
         jointScript3 = joint3.GetComponent<articulationRotoideScript>();
         jointScript4 = joint4.GetComponent<articulationPrismaticScript>();
+        jointScript5 = joint5.GetComponent<articulationRotoideScript>();
     }
 
     // Update is called once per frame
@@ -67,6 +74,10 @@ public class RobotScript : MonoBehaviour
         if (jointsPositions.positionPrismatic2 != jointScript4.distance)
         {
             jointScript4.distance = jointsPositions.positionPrismatic2;
+        }
+        if (jointsPositions.positionRotoide3 != jointScript5.angle)
+        {
+            jointScript5.angle = jointsPositions.positionRotoide3;
         }
     }
 }
